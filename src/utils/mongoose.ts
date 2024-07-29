8ad217f7f4acb99d6ad5a827fb0b519af59ae662
JaredvGraham <jgram153@icloud.com>
2024-07-29 18:39:02 -0400

    mongo timeout error

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -8,15 +8,18 @@ const MONGO_URI = process.env.MONGO_URI;
 
 export async function connect() {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout for initial connection
+      socketTimeoutMS: 45000, // 45 seconds timeout for sockets
+    });
 
     mongoose.connection.on("connected", () => {
       console.log("Connected to MongoDB");
+    });
 
-      mongoose.connection.on("error", (err) => {
-        console.log("MongoDB connection error: " + err);
-        process.exit();
-      });
+    mongoose.connection.on("error", (err) => {
+      console.log("MongoDB connection error: " + err);
+      process.exit();
     });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
